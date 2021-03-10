d9c46cf839c3cba0ef110c535a36c15e0c9cf655
DESKTOP-S6VJFGE\johan <johannes.mueller88@hotmail.de>
2021-03-10 12:46:41 +0100

    Unet created according to https://amaarora.github.io/2020/09/13/unet.html

---
+++ Unet.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 11:37:46 2021
+
+@author: johan
+"""
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3)  # create feature maps
+        self.relu  = nn.ReLU()  # ReLu activation function
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3)  # downsample feature maps
+    
+    def forward(self, x):
+        return self.relu(self.conv2(self.relu(self.conv1(x))))
+    
+# Contractive part of Unet
+class Encoder(nn.Module):
+    def __init__(self, chs=(3,64,128,256,512,1024)):
+        super().__init__()
+        
+        # create (encoder) list of convolutions and downsampling (MaxPool)
+        Blocks = [Block(chs[i], chs[i+1]) for i in range(len(chs)-1)]
+        self.enc_blocks = nn.ModuleList(Blocks)
+        self.pool       = nn.MaxPool2d(2)
+    
+    def forward(self, x):
+        
+        ftrs = []
+        for block in self.enc_blocks:
+            x = block(x)
+            ftrs.append(x)
+            x = self.pool(x)
+        return ftrs
+
+
+# Expansive part of network
+class Decoder(nn.Module):
+    def __init__(self, chs=(1024, 512, 256, 128, 64)):  # reverse of encoder
+        super().__init__()
+        
+        UpConvs = [nn.ConvTranspose2d(chs[i], chs[i+1], 2, 2) for i in range(len(chs)-1)]
+        Blocks = [Block(chs[i], chs[i+1]) for i in range(len(chs)-1)]
+        
+        self.chs         = chs
+        self.upconvs    = nn.ModuleList(UpConvs)
+        self.dec_blocks = nn.ModuleList(Blocks) 
+        
+    def forward(self, x, encoder_features):
+        
+        for i in range(len(self.chs)-1):
+            x        = self.upconvs[i](x)  #  Up-convolution
+            enc_ftrs = self.crop(encoder_features[i], x)
+            x        = torch.cat([x, enc_ftrs], dim=1)
+            x        = self.dec_blocks[i](x)
+        return x
+    
+    def crop(self, enc_ftrs, x):
+        _, _, H, W = x.shape
+        enc_ftrs   = torchvision.transforms.CenterCrop([H, W])(enc_ftrs)
+        return enc_ftrs
+
+
+class UNet(nn.Module):
+    def __init__(self,
+                 enc_chs=(3,64,128,256,512,1024),
+                 dec_chs=(1024, 512, 256, 128, 64),
+                 num_class=1,
+                 retain_dim=False,
+                 out_sz=(572,572)):
+        super().__init__()
+        self.encoder     = Encoder(enc_chs)
+        self.decoder     = Decoder(dec_chs)
+        self.head        = nn.Conv2d(dec_chs[-1], num_class, 1)
+        self.retain_dim  = retain_dim
+        
+        self.out_sz = out_sz
+
+    def forward(self, x):
+        enc_ftrs = self.encoder(x)
+        out      = self.decoder(enc_ftrs[::-1][0], enc_ftrs[::-1][1:])
+        out      = self.head(out)
+        if self.retain_dim:
+            out = F.interpolate(out, self.out_sz)
+        return out
