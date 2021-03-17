f01c37f6bfc8b03e02b4f6868fcacbedbec76e12
DESKTOP-S6VJFGE\johan <johannes.mueller88@hotmail.de>
2021-03-18 00:02:38 +0100

    new Loss and Evaluation metric

--- Train.py
+++ Train.py
@@ -23,7 +23,7 @@ import segmentation_models_pytorch as smp
 from Utils import helper_functions
 
 from torch.utils.data import DataLoader
-# from Utils.Losses import ComboLoss, MixedLoss, LovaszLossSigmoid
+from sklearn.metrics import jaccard_score
 import torch
 from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss
 
@@ -39,72 +39,39 @@ class Dataset():
         
         mask_path = os.path.join(self.image_base_dir, image_id)
         img_path = mask_path.replace('-labelled', '')
-        image     = cv2.imread(img_path, 1)
-        mask      = np.argmax(tf.imread(mask_path), axis=0)
+        image = cv2.imread(img_path, 1).transpose((2,0,1)).astype(float)
+        image[:,(np.sum(image, axis=0) == 0)] = 255
         
-        # apply augmentations
-        if self.augmentation:
-            sample = {"image": image, "mask": mask}
-            sample = self.augmentation(image=image, mask=mask)
-            image, mask = sample['image'], sample['mask']
-
+        mask = np.argmax(tf.imread(mask_path), axis=0)
+        mask = mask[None, :, :]
+        
+        
         return {'image': image, 'mask' : mask}
         
     def __len__(self):
         return len(self.image_ids)
 
 
-def train_one_epoch(train_loader, model, optimizer, loss_fn, accumulation_steps=1, device='cuda'):
-    losses = helper_functions.AverageMeter()
-    model = model.to(device)
-    model.train()
-    
-    if accumulation_steps > 1: 
-        optimizer.zero_grad()
-    tk0 = tqdm(train_loader, total=len(train_loader))
-    
-    for b_idx, data in enumerate(tk0):
-        for key, value in data.items():
-            data[key] = value.to(device)
-            
-        if accumulation_steps == 1 and b_idx == 0:
-            optimizer.zero_grad()
-
-        out  = model(data['image'])
-        loss = loss_fn(out, torch.argmax(data['mask'], dim=1))
-        
-        # backpropagation
-        with torch.set_grad_enabled(True):
-            loss.backward()
-            if (b_idx + 1) % accumulation_steps == 0:
-                optimizer.step()
-                optimizer.zero_grad()
-                
-        # update weights
-        losses.update(loss.item(), train_loader.batch_size)
-        tk0.set_postfix(loss=losses.avg, learning_rate=optimizer.param_groups[0]['lr'])
-    return losses.avg
-
-
-
 root = os.getcwd()
-src = root + r'\src\QuPath_Tiling\tiles\N182a_SAS_1_1_170905-Scene-1-ScanRegion0_HE'
+src = root + r'\src\QuPath_Tiling\tiles'
 
 # Config
 FOLD_ID = 4
-TRAIN_BATCH_SIZE = 6
-VALID_BATCH_SIZE = 2
+BATCH_SIZE = 6
 USE_SAMPLER = False
 SAMPLER  = None
 num_workers = 0
 LEARNING_RATE = 2e-5
-criterion = BCEWithLogitsLoss()
+criterion = CrossEntropyLoss()
 TRAIN_MODEL = True
 EPOCHS = 300
 IMG_SIZE = 512
 EVALUATE = True
 N_CLASSES = 3
-PATIENCE = 20
+PATIENCE = 30
+device= 'cuda'
+
+styles = ['--', '-', ':']
 
 
 if __name__ == '__main__':
@@ -122,7 +89,9 @@ if __name__ == '__main__':
         classes=N_CLASSES, 
         activation=None,
     )
-        
+    
+    model = model.to(device)
+    model.train()        
     
     optimizer = torch.optim.Adam(model.parameters(), lr= LEARNING_RATE)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(
@@ -136,54 +105,82 @@ if __name__ == '__main__':
     
     train_score = []
     test_score = []
-    h_train, h_test, h_legend = None, None, None
+    h_train, h_legend = None, None
+    h_test  = [None for x in range(N_CLASSES)]
     flag = True
+    
+    # create 5 folds train/test groups
+    kf = KFold(n_splits=5, shuffle=True)
+    df['kfold']=-1
+    for fold, (train_index, test_index) in enumerate(kf.split(df.Image_ID)):
+            df.loc[test_index, 'kfold'] = fold
+    
+    # single fold training for now, rerun notebook to train for multi-fold
+    TRAIN_DF = df.query(f'kfold!={FOLD_ID}').reset_index(drop=True)
+    VAL_DF   = df.query(f'kfold=={FOLD_ID}').reset_index(drop=True)
+    
+    # train dataset
+    train_dataset = Dataset(TRAIN_DF, src)
+    val_dataset   = Dataset(VAL_DF, src)
+    
+    # dataloaders
+    train_dataloader = DataLoader(train_dataset, BATCH_SIZE, shuffle=True, num_workers=num_workers)
+    val_dataloader   = DataLoader(val_dataset, BATCH_SIZE, shuffle=False, num_workers=num_workers)
     
     
     if TRAIN_MODEL:
         for epoch in range(EPOCHS):
             
-            # create 5 folds train file
-            kf = KFold(n_splits=5, shuffle=True)
-            df['kfold']=-1
-            for fold, (train_index, test_index) in enumerate(kf.split(df.Image_ID)):
-                    df.loc[test_index, 'kfold'] = fold
-            df.to_csv(DIRS['KFOLD'], index=False)
+            # train
+            optimizer.zero_grad()
+            tk0 = tqdm(train_dataloader, total=len(train_dataloader))
             
-            # single fold training for now, rerun notebook to train for multi-fold
-            df = pd.read_csv(DIRS['KFOLD'])
-            TRAIN_DF = df.query(f'kfold!={FOLD_ID}').reset_index(drop=True)
-            VAL_DF   = df.query(f'kfold=={FOLD_ID}').reset_index(drop=True)
-            
-            # Train transforms
-            TFMS = albu.Compose([albu.HorizontalFlip(),
-                                 albu.Rotate(10),
-                                 albu.Normalize(),
-                                 ToTensor()])
-            
-            # Test transforms
-            TEST_TFMS = albu.Compose([albu.Normalize(), ToTensor(), ])
-            
-            # train dataset
-            train_dataset = Dataset(TRAIN_DF, src, TFMS)
-            val_dataset   = Dataset(VAL_DF, src, TEST_TFMS)
-            
-            # dataloaders
-            train_dataloader = DataLoader(train_dataset, TRAIN_BATCH_SIZE, shuffle=True, 
-                                          num_workers=num_workers)
-            val_dataloader   = DataLoader(val_dataset, VALID_BATCH_SIZE, shuffle=False, num_workers=num_workers)
-            
-            
-            loss = train_one_epoch(train_dataloader, model, optimizer, criterion)
-            dice = helper_functions.evaluate(val_dataloader, model,
-                                             metric=helper_functions.metric,
-                                             n_classes=N_CLASSES)
-            
+            for b_idx, data in enumerate(tk0):
+                
+                # move images on GPU
+                for key, value in data.items():
+                    data[key] = value.to(device).float()
+                    
+                # train
+                # helper_functions.visualize_batch(data)
+                out  = model(data['image'])
+                loss = criterion(out, data['mask'][:, 0].long())
+                loss.backward()
+                optimizer.step()
+                tk0.set_postfix(loss=loss.cpu().detach().numpy())
+
+            # evaluate
+            with torch.no_grad():
+                tk1 = tqdm(val_dataloader, total=len(val_dataloader))
+                dice = np.zeros(N_CLASSES)
+                for b_idx, data in enumerate(tk1):
+                    
+                    # Eval
+                    out = model(data['image'].to(device).float())                 
+                    out = torch.argmax(out, dim=1).view(-1)
+                    mask = data['mask'].view(-1)
+                    
+                    dice += jaccard_score(mask.cpu(), out.cpu(), average=None)
+                
+                dice /= b_idx
+                tk1.set_postfix(score=np.mean(dice))
+                    
             train_score.append(loss)
             test_score.append(dice)
                 
             h_train = ax.plot(train_score, color='orange', label='Training loss')
-            h_test = ax1.plot(test_score, color='blue', label='Dice validation score')
+            
+            try:
+                [ax1.lines.pop(i) for i in range(N_CLASSES)]
+            except Exception:
+                pass
+                
+            for i in range(len(dice)):
+                score = np.concatenate( test_score, axis=0 ).reshape(-1, 3)
+                h_test[i] = ax1.plot(score[:,i], color='blue',
+                                     label='Dice score label {:d}'.format(i),
+                                     linestyle=styles[i])
+
             if flag:
                 h_legend = ax.legend()
                 h_legend1 = ax1.legend(loc='upper center')
@@ -192,8 +189,9 @@ if __name__ == '__main__':
             plt.pause(1)
             
             scheduler.step()
+            dice = np.mean(dice)
             print(f"EPOCH: {epoch}, TRAIN LOSS: {loss}, VAL DICE: {dice}")
-            es(dice, model, model_path=f"{DIRS['EXP_DIR']}/model/bst_model{IMG_SIZE}_fold{FOLD_ID}_{np.round(dice,4)}.bin")
+            es(dice, model, model_path=f"{DIRS['EXP_DIR']}/model/bst_model{IMG_SIZE}_fold{FOLD_ID}_{np.round(dice, 4)}.bin")
             best_model = f"../data/bst_model{IMG_SIZE}__fold{FOLD_ID}_{np.round(es.best_score,4)}.bin"
             if es.early_stop:
                 print('\n\n -------------- EARLY STOPPING -------------- \n\n')
