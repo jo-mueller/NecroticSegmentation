47b645af88b50259786871a18f4ce2209a02b180
DESKTOP-S6VJFGE\johan <johannes.mueller88@hotmail.de>
2021-03-21 23:51:50 +0100

    Added stratified kfold to training

--- Train.py
+++ Train.py
@@ -11,21 +11,17 @@ import numpy as np
 import pandas as pd
 import tifffile as tf
 import cv2
-import tifffile as tf
 import matplotlib.pyplot as plt
 
-import albumentations as albu
-from albumentations.pytorch.transforms import ToTensor
-
-from sklearn.model_selection import KFold
-
+from sklearn.model_selection import StratifiedKFold
 import segmentation_models_pytorch as smp
 from Utils import helper_functions
 
 from torch.utils.data import DataLoader
 from sklearn.metrics import jaccard_score
 import torch
-from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss
+from torch.nn import CrossEntropyLoss
+import albumentations as A
 
 class Dataset():
     def __init__(self, rle_df, image_base_dir, augmentation=None):
@@ -39,14 +35,20 @@ class Dataset():
         
         mask_path = os.path.join(self.image_base_dir, image_id)
         img_path = mask_path.replace('-labelled', '')
-        image = cv2.imread(img_path, 1).transpose((2,0,1)).astype(float)
+        image = cv2.imread(img_path, 1).astype(float)
         image[:,(np.sum(image, axis=0) == 0)] = 255
         
+        # mask = tf.imread(mask_path)
         mask = np.argmax(tf.imread(mask_path), axis=0)
-        mask = mask[None, :, :]
+        # mask = mask[None, :, :]
+        
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask= sample['image'], sample['mask']
+            
         
         
-        return {'image': image, 'mask' : mask}
+        return {'image': image.transpose((2,0,1)), 'mask' : mask[None,: , :]}
         
     def __len__(self):
         return len(self.image_ids)
@@ -64,12 +66,13 @@ num_workers = 0
 LEARNING_RATE = 2e-5
 criterion = CrossEntropyLoss()
 TRAIN_MODEL = True
-EPOCHS = 300
+EPOCHS = 150
 IMG_SIZE = 512
 EVALUATE = True
 N_CLASSES = 3
-PATIENCE = 30
+PATIENCE = 40
 device= 'cuda'
+keep_chekpoints = True
 
 styles = ['--', '-', ':']
 
@@ -79,9 +82,8 @@ if __name__ == '__main__':
     # Create paths for train/test image data
     DIRS = helper_functions.createExp_dir(root + '/data')  
     
-    # Read labels to dataframe with RLE encoding
-    df = helper_functions.scan_directory(src, img_type='-labelled')
-    # df = helper_functions.rle_directory(src, outname=DIRS['RLE_DATA'])
+    # Read label tiles to dataframe
+    df = helper_functions.scan_directory(src, img_ID='-labelled')
     
     model = smp.Unet(
         encoder_name='resnet50', 
@@ -91,13 +93,48 @@ if __name__ == '__main__':
     )
     
     model = model.to(device)
-    model.train()        
+    model.train()
+    
+    aug_train = A.Compose([
+        A.VerticalFlip(p=0.5),              
+        A.RandomRotate90(p=0.5),
+        A.OneOf([
+            A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.5),
+            A.GridDistortion(p=0.5)         
+        ], p=0.8)
+    ])
+    
+    aug_test = A.Compose([
+        # A.Normalize()
+        # A.ToTensorV2()
+    ])
+
+    
+
     
     optimizer = torch.optim.Adam(model.parameters(), lr= LEARNING_RATE)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(
         optimizer, milestones=[3,5,6,7,8,9,10,11,13,15], gamma=0.75)
 
     es = helper_functions.EarlyStopping(patience=PATIENCE, mode='max')
+    
+    # create 5 folds train/test groups
+    kf = StratifiedKFold(n_splits=5, shuffle=True)
+    df['kfold']=-1
+    for fold, (train_index, test_index) in enumerate(kf.split(X = df.Image_ID, y=df.has_all_labels)):
+            df.loc[test_index, 'kfold'] = fold
+    
+    # single fold training for now, rerun notebook to train for multi-fold
+    TRAIN_DF = df.query(f'kfold!={FOLD_ID}').reset_index(drop=True)
+    VAL_DF   = df.query(f'kfold=={FOLD_ID}').reset_index(drop=True)
+    
+    # train/validation dataset
+    train_dataset = Dataset(TRAIN_DF, src, aug_train)
+    val_dataset   = Dataset(VAL_DF, src, aug_test)
+    
+    # dataloaders
+    train_dataloader = DataLoader(train_dataset, BATCH_SIZE, shuffle=True, num_workers=num_workers)
+    val_dataloader   = DataLoader(val_dataset, BATCH_SIZE, shuffle=False, num_workers=num_workers)
     
     fig, ax = plt.subplots(nrows=1, ncols=1)
     ax1 = ax.twinx()
@@ -109,25 +146,6 @@ if __name__ == '__main__':
     h_test  = [None for x in range(N_CLASSES)]
     flag = True
     
-    # create 5 folds train/test groups
-    kf = KFold(n_splits=5, shuffle=True)
-    df['kfold']=-1
-    for fold, (train_index, test_index) in enumerate(kf.split(df.Image_ID)):
-            df.loc[test_index, 'kfold'] = fold
-    
-    # single fold training for now, rerun notebook to train for multi-fold
-    TRAIN_DF = df.query(f'kfold!={FOLD_ID}').reset_index(drop=True)
-    VAL_DF   = df.query(f'kfold=={FOLD_ID}').reset_index(drop=True)
-    
-    # train dataset
-    train_dataset = Dataset(TRAIN_DF, src)
-    val_dataset   = Dataset(VAL_DF, src)
-    
-    # dataloaders
-    train_dataloader = DataLoader(train_dataset, BATCH_SIZE, shuffle=True, num_workers=num_workers)
-    val_dataloader   = DataLoader(val_dataset, BATCH_SIZE, shuffle=False, num_workers=num_workers)
-    
-    
     if TRAIN_MODEL:
         for epoch in range(EPOCHS):
             
@@ -135,6 +153,7 @@ if __name__ == '__main__':
             optimizer.zero_grad()
             tk0 = tqdm(train_dataloader, total=len(train_dataloader))
             
+            visualize_flag = True
             for b_idx, data in enumerate(tk0):
                 
                 # move images on GPU
@@ -142,12 +161,20 @@ if __name__ == '__main__':
                     data[key] = value.to(device).float()
                     
                 # train
-                # helper_functions.visualize_batch(data)
-                out  = model(data['image'])
-                loss = criterion(out, data['mask'][:, 0].long())
+                
+                data['prediction']  = model(data['image'])
+                loss = criterion(data['prediction'], data['mask'][:, 0].long())
                 loss.backward()
                 optimizer.step()
                 tk0.set_postfix(loss=loss.cpu().detach().numpy())
+                
+                # try:
+                #     if np.mean(test_score[-1]) > 0.65 and visualize_flag:
+                        
+                #         helper_functions.visualize_batch(data)
+                #         visualize_flag = False
+                # except Exception:
+                #     pass
 
             # evaluate
             with torch.no_grad():
@@ -161,12 +188,11 @@ if __name__ == '__main__':
                     mask = data['mask'].view(-1)
                     
                     dice += jaccard_score(mask.cpu(), out.cpu(), average=None)
+                    tk1.set_postfix(score=np.mean(dice))
                 
-                dice /= b_idx
-                tk1.set_postfix(score=np.mean(dice))
-                    
-            train_score.append(loss)
-            test_score.append(dice)
+                dice /= len(tk1)
+                train_score.append(loss.cpu().detach())
+                test_score.append(dice)
                 
             h_train = ax.plot(train_score, color='orange', label='Training loss')
             
@@ -197,6 +223,13 @@ if __name__ == '__main__':
                 print('\n\n -------------- EARLY STOPPING -------------- \n\n')
                 break
 
-
+    # # keep only most recent model
+    # if not keep_chekpoints:
+    #     models =  os.listdir(DIRS['Models'])
+    #     models = [os.path.join(DIRS['Models'], x) for x in models]
+    #     for model in models:
+    #         if not model
+    #     [os.path.remove(os.path.join(DIRS['Models'] + model))]
+    
 
 
