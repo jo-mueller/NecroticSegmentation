47e955c0a9cc43de843bd9929a743971510d7d82
DESKTOP-S6VJFGE\johan <johannes.mueller88@hotmail.de>
2021-07-16 16:28:51 +0200

    added generator script for training data based on previously segmented data

--- Utils/Generate_IF_tiles.py
+++ Utils/Generate_IF_tiles.py
@@ -6,46 +6,67 @@ Created on Wed Jul 14 12:18:26 2021
 """
 
 import os
-import cv2
-import pandas as pd
+from tqdm import tqdm
 import numpy as np
+import pandas as pd
 import tifffile as tf
 import matplotlib.pyplot as plt
 import aicsimageio
+import aicspylibczi
 
-# def tilify(sample_dir):
+def tilify(image, mask, name, outdir, tsize=256):
     
-   
+    tiles_x = image.shape[1]//tsize
+    tiles_y = image.shape[2]//tsize
+    
+    for i in tqdm(range(tiles_x), desc=name):
+        for j in range(tiles_y):
+            img_patch = image[:,
+                              i*tsize: i*tsize + tsize,
+                              j*tsize: j*tsize + tsize]
+            msk_patch = mask[i*tsize: i*tsize + tsize,
+                              j*tsize: j*tsize + tsize]
             
+            # No tiles with majority (>95%) background
+            if np.sum(msk_patch == 1) >0.95*tsize**2:
+                continue
+            
+            # No tiles with weird background zeros
+            if np.sum(img_patch.sum(axis=0) == 0) > 100:
+                continue
+            
+            # format string for tile location
+            position = '[x={:d} y={:d} w={:d} h={:d}]'.format(i, j, tsize, tsize)
+            
+            # write tile to file
+            tf.imwrite(os.path.join(outdir, name + ' ' + position + '.tif'), img_patch)
+            tf.imwrite(os.path.join(outdir, name + ' ' + position + '-labelled.tif'), msk_patch)
     
     
 if __name__ == '__main__':
-    data_dir = r'E:\Promotion\Projects\2020_Radiomics\Data'
-    n_samples= 20
+    data_dir = r'E:\Promotion\Projects\2021_Necrotic_Segmentation\src\IF\raw'
+    tile_dir = os.path.join(os.path.dirname(data_dir), 'tiles')    
     
-    samples = []
-    for root, subdirs, files in os.walk(data_dir):
-        if "Histology" in root and "1_seg" in subdirs:
-            
-            # no censored data
-            if "censored" in root:
-                continue
-            
-            samples.append(root)
+    mask_ID = '-labelled'    
+    files = os.listdir(data_dir)
     
-    # draw some random samples from the data pool
-    samples = np.asarray(samples)[np.random.randint(0, len(samples), n_samples)]
+    df = pd.DataFrame(columns=['Image', 'Mask'])
+    df.Mask = [f for f in files if mask_ID in f]
+    df.Image = [f.replace(mask_ID, '').replace('.tif', '.czi') for f in df.Mask]
     
-    for sample in samples:
-        try:
-            img = [x for x in os.listdir(sample) if "IF" in x and (x.endswith('tif') or x.endswith('czi'))][0]
-        except:
-            continue
+    # irerate over samples in dataframe
+    for i, sample in df.iterrows():
+        czi = aicspylibczi.CziFile(os.path.join(data_dir, sample.Image))
+        C0 = czi.read_mosaic(C=0, scale_factor=1)[0][None, :, :]  # read channel and add channel dimension
+        C1 = czi.read_mosaic(C=1, scale_factor=1)[0][None, :, :]
+        C2 = czi.read_mosaic(C=2, scale_factor=1)[0][None, :, :]
+        Image = np.vstack([C0, C1, C2])
+        Mask = tf.imread(os.path.join(data_dir, sample.Mask))
         
-        if img.endswith('tif'):
-            img = tf.imread(os.path.join(sample, img))
-        elif img.endswith('czi'):
-            AICS = aicsimageio.AICSImage(os.path.join(sample, img))
-            
-            
-        mask = tf.imread(os.path.join(sample, "1_seg", "IF_seg_Simple_Segmentation.tif"))
+        tilify(Image, Mask,
+               name=sample.Image.split('.')[0],
+               outdir=tile_dir)
+    
+    
+    
+    
