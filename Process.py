92d0f4d50d1f76ce4e4ffe492b0f7279dbf73033
Johannes Müller <johannes.mueller88@hotmail.de>
2021-07-16 16:31:28 +0200

    changed forward processing scheme to location-based tile seclection

--- Process.py
+++ Process.py
@@ -30,28 +30,27 @@ import torch
 from torch import nn
 
 class InferenceDataset():
-    def __init__(self, image_base_dir, filename, augmentation=None,
-                 patch_size=512, stride=16, n_classes=3, series=3, target_pixsize = 2.0,
-                 max_offset=128, n_offsets=3):
+    def __init__(self, filename, n_classes, **kwargs):
         
-        self.image_base_dir = image_base_dir
-        self.filename = os.path.join(image_base_dir, filename)
-        self.augmentation = augmentation
+        self.resolution = kwargs.get('resolution', 0.4418)
+        self.target_pixsize = kwargs.get('target_pixsize', 2.5)
+        self.patch_size = kwargs.get('patch_size', 128)
+        self.stride = kwargs.get('stride', 16)
+        self.density = kwargs.get('density', 16)
+        self.augmentation = kwargs.get('augmentation', None)
+        self.batch_size = kwargs.get('batch_size', 20)
+        self.device = kwargs.get('device', 'cuda')
+        
+        self.filename = filename
         self.resolution = 0.4418
         self.n_classes = n_classes
         
-        self.patch_size = int(patch_size)
-        self.stride = int(stride)
-        self.inner = int(patch_size - 2*stride)
-        self.offset = 0
-        self.max_offset = max_offset
-        self.n_offsets = n_offsets
         
         # Read czi image
-        czi = aicspylibczi.CziFile(self.filename)
-        self.image = czi.read_mosaic(C = 0, scale_factor=self.resolution/target_pixsize)[0]
+        self.czi = aicspylibczi.CziFile(self.filename)
+        self.image = self.czi.read_mosaic(C = 0, scale_factor=self.resolution/self.target_pixsize)[0]
         self.image = self.image[:, :, ::-1]
-        self.resolution = target_pixsize
+        self.resolution = self.target_pixsize
         # self.resample(target_pixsize)
         
         # transpose if necessary
@@ -62,153 +61,80 @@ class InferenceDataset():
                                    dtype='float32')
         
         # assign indeces on 2d grid and pad to prevent index errors
-        self.create_index_map()
-        self.pad()
+        self.create_samplelocations()
+        # self.create_index_map()
+        # self.pad()
         
-    def pad(self):
+    def create_samplelocations(self):
         """
-        Expands image to have a size that's a integer factor of the tile size
+        Prepares a list of locations where the image will be fed forward
+        through the network
         """
         
-        # see how many tiles fit in the raw image and round up
-        self.dimensions = self.image.shape
-        shape = np.ceil(np.asarray(self.image.shape)/self.inner) * self.inner
-        shape[np.argmin(shape)] = 3  # force channel dimension to 3
+        # create sampling locations, omit half-tile sized margin at image edge
+        X = np.arange(self.patch_size//2, self.image.shape[1] - self.patch_size//2, self.density)
+        Y = np.arange(self.patch_size//2, self.image.shape[2] - self.patch_size//2, self.density)
         
-        #  difference in shape
-        ds = shape - self.image.shape
+        self.locations = []
+        for x in tqdm(X, desc='Browsing image...'):
+            for y in Y:
+                patch = self.image[:
+                                   x - self.patch_size//2 : x + self.patch_size//2,
+                                   y - self.patch_size//2 : y + self.patch_size//2]
+                if np.sum(patch.sum(axis=0) == 0) > 50 or np.sum(patch.sum(axis=0) >= 3*354):
+                    continue
+                self.locations.append([x, y])
         
-        # preserve old shape for later cropping
-        width = self.image.shape[1]
-        height = self.image.shape[2]
-        self.anchor = [0, 0, width, height]
-        
-        # calculate left/right/top/down padding margins
-        # Add offset to prevent overshoot at edges
-        x = 0
-        xx = int(ds[1] - x) + self.max_offset
-        
-        y = 0
-        yy = int(ds[2] - y) + self.max_offset
-        
-        # Execute padding
-        self.image = np.pad(self.image, ((0,0), (x, xx), (y, yy)),
-                            mode='constant', constant_values=0)
-        self.prediction = np.pad(self.prediction, ((0,0), (x, xx), (y, yy)),
-                            mode='constant', constant_values=0)
-        
-        
-    def create_index_map(self):
-        
-        shape = ((np.array(self.image.shape[1:]))//self.inner).astype(int)
-        
-        self.index_map = np.arange(0, shape[0] * shape[1], 1).astype(np.float32)
-        np.random.shuffle(self.index_map)
-        self.index_map = self.index_map.reshape(shape)
-        
-        # Check which of the locations refer to dumb background areas
-        for i in range(self.__len__()):
-            patch = np.sum(self[i]['image'], axis=0)
-            
-            if np.sum(patch == 0) > 100 or np.sum(patch >= 3*254) > 100:
-                self.index_map[self.index_map == i] = np.nan
-        
-        # Now, fill the deleted indices so that a continuous range of indices is provided
-        idx = 0
-        while True:
-            if idx == np.sum(~np.isnan(self.index_map)):
-                break
-            if (self.index_map == idx).any():
-                idx += 1
-                continue
-            else:
-                self.index_map[self.index_map >= idx] -= 1
-                idx = 0
-        
-        return 1
         
     def __len__(self):
-        return int(np.nanmax(self.index_map) + 1)
+        return len(self.locations)
     
     def __getitem__(self, key):
         
-        try:
-            ind = np.argwhere(self.index_map == key)[0]
-        except Exception:
-            pass
-        i, j = ind[0], ind[1]
+        loc = self.locations[key]
+        x, y = loc[0], loc[1]
         
-        inner = self.inner
-        offset = self.offset
-       
-        image = self.image[:, 
-                           offset + i*inner: offset + i*inner + inner,
-                           offset + j*inner: offset + j*inner + inner]
-        
-        if self.augmentation:
-            sample = self.augmentation(image=image.transpose((1,2,0)))
-            image = sample['image'].transpose((2,0,1))
-
-        return {'image': image}
+        patch = self.image[:
+                           x - self.patch_size//2 : x + self.patch_size//2,
+                           y - self.patch_size//2 : y + self.patch_size//2]
+            
+        return {'image': patch}
     
     def __setitem__(self, key, value):
-        try:
-            ind = np.argwhere(self.index_map == key)[0]
-        except Exception:
-            pass
-        i, j = ind[0], ind[1]
         
         # crop center of processed tile
         patch = value[:,
-                      self.stride : self.patch_size - self.stride,
-                      self.stride : self.patch_size - self.stride]
+                      self.stride : - self.stride,
+                      self.stride : - self.stride]
         
-        inner = self.inner
-        offset = self.offset
+        size = patch.shape[1]
+        x, y = self.locations[key]
         
         self.prediction[:,
-                        offset + i*inner : offset + i*inner + inner,
-                        offset + j*inner : offset + j*inner + inner] += patch
-        
+                        x - size//2 : x + size//2,
+                        y - size//2 : y + size//2] += patch
             
-    def predict(self, model, device='cuda', batch_size=6):
-                
-        if self.n_offsets == 0:
-            offsets = [0]
-        else:
-            offsets = np.arange(0, self.max_offset)[::int(self.max_offset/self.n_offsets)]
-        
+    def predict(self, model):
+
         with torch.no_grad():
-            
-            tk0 = tqdm(offsets, desc=os.path.basename(self.filename), total=len(offsets))
-            for offset in tk0:
+            dataloader = DataLoader(self, batch_size=self.batch_size,
+                                    shuffle=False, num_workers=0)        
+            tk0 = tqdm(dataloader, total=self.__len__(), desc='Tilewise forward segmentation...')
+            for b_idx, data in enumerate(tk0):
+                    
+                data['image'] = data['image'].to(self.device).float()
+                data['prediction'] = model(data['image'])
+                out = torch.sigmoid(data['prediction']).detach().cpu().numpy()
                 
-                self.offset = offset
-                dataloader = DataLoader(self, batch_size=batch_size,
-                        shuffle=False, num_workers=0)
-    
-                # iterate over dataloader
-                for i, data in enumerate(dataloader):
-                    
-                    data['image'] = data['image'].to(device).float()
-                    data['prediction'] = model(data['image'])
-                    out = torch.sigmoid(data['prediction']).detach().cpu().numpy()
-                    
-                    tk0.set_postfix(offset=offset, batch=i)
-                    
-                    # helper_functions.visualize_batch(data, 1, 1, 1)
-                    
-                    # iteratively write results from batches to prediction map
-                    for b_idx in range(out.shape[0]):
-                        self[i*batch_size + b_idx] = out[b_idx]
+                # iteratively write results from batches to prediction map
+                for b_idx in range(out.shape[0]):
+                    self[i*batch_size + b_idx] = out[b_idx]
                         
         # average predictions and undo padding
         self.prediction /= (self.n_offsets + 1)
         self.prediction = self.prediction[:,
                                           self.anchor[0]: self.anchor[0] + self.anchor[2],
                                           self.anchor[1]: self.anchor[1] + self.anchor[3]]
-        
-        
         
     def postprocess(self, Class_cutoffs=[0.5, 0.5, 0.4]):
         """
